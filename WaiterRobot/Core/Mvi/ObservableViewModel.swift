f1d32ad9184ef41037d8a59a295c82bfa82063e1
Fabian Schedler <dev.fabian.schedler@gmail.com>
2022-11-26 20:06:01 +0100

    Add ObservableViewModel wrapper

---
+++ WaiterRobot/Core/Mvi/ObservableViewModel.swift
@@ -0,0 +1,31 @@
+/// Base on
+/// - https://johnoreilly.dev/posts/kotlinmultiplatform-swift-combine_publisher-flow/
+/// - https://proandroiddev.com/kotlin-multiplatform-mobile-sharing-the-ui-state-management-a67bd9a49882
+/// - https://github.com/orbit-mvi/orbit-swift-gradle-plugin/blob/main/src/main/resources/stateObject.swift.mustache
+
+import Foundation
+import Combine
+import shared
+
+@MainActor
+class ObservableViewModel<S: ViewModelState, E: ViewModelEffect, VM: AbstractViewModel<S, E>>: ObservableObject {
+  @Published public private(set) var state: S
+  public private(set) var sideEffect: AnyPublisher<E, Never>
+  
+  public let actual: VM
+  
+  init(vm: VM) {
+    self.actual = vm
+    // This is save, as the constraint is required by the generics (S must be the state of the provided VM)
+    self.state = actual.container.stateFlow.value as! S
+    self.sideEffect = actual.container.sideEffectFlow.asPublisher() as AnyPublisher<E, Never>
+    
+    (actual.container.stateFlow.asPublisher() as AnyPublisher<S, Never>)
+      .receive(on: RunLoop.main)
+      .assign(to: &$state)
+  }
+  
+  deinit {
+    actual.onCleared()
+  }
+}
