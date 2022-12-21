ae958db74642addbcfd315e28d1f284a3a00d3ff
Fabian Schedler <dev.fabian.schedler@gmail.com>
2022-12-21 23:32:45 +0100

    Add Select/SwitchEvent Screen

--- WaiterRobot/Ui/SwitchEvent/SwitchEventScreen.swift
+++ WaiterRobot/Ui/SwitchEvent/SwitchEventScreen.swift
@@ -1,14 +1,70 @@
 import SwiftUI
+import UIPilot
+import shared
 
 struct SwitchEventScreen: View {
-    var body: some View {
-        // TODO
-        Text("Hello, World!")
+  @EnvironmentObject var navigator: UIPilot<Screen>
+  
+  @StateObject private var strongVM = ObservableViewModel(vm: koin.switchEventVM())
+  
+  @SwiftUI.State private var selectedEvent: Event? = nil
+  
+  var body: some View {
+    unowned let vm = strongVM
+    
+    ScreenContainer(vm.state) {
+      VStack {
+        Image(systemName: "person.3")
+          .resizable()
+          .scaledToFit()
+          .frame(maxHeight: 100)
+          .padding()
+        
+        Text(S.switchEvent.desc())
+          .multilineTextAlignment(.center)
+          .padding()
+        
+        Divider()
+        
+        if vm.state.events.isEmpty {
+          Text(S.switchEvent.noEventFound())
+            .multilineTextAlignment(.center)
+            .padding()
+          
+          Spacer()
+        } else {
+          ScrollView {
+            LazyVStack {
+              ForEach(vm.state.events, id: \.id) { event in
+                Button {
+                  vm.actual.onEventSelected(event: event)
+                } label: {
+                  Event(event: event)
+                    .padding(.horizontal)
+                }.foregroundColor(Color("textColor"))
+                Divider()
+              }
+            }
+          }
+        }
+      }
+      .navigationBarHidden(true)
+      .onReceive(vm.sideEffect) { effect in
+        switch effect {
+        case let navEffect as NavigationEffect:
+          handleNavigation(navEffect.action, navigator)
+        default:
+          koin.logger(tag: "LoginScannerScreen").w {
+            "No action defined for sideEffect \(effect.self.description)"
+          }
+        }
+      }
     }
+  }
 }
 
 struct SwitchEventScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        SwitchEventScreen()
-    }
+  static var previews: some View {
+    SwitchEventScreen()
+  }
 }
