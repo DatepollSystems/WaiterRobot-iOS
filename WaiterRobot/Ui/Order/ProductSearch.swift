52e4550c89e6dce3a55de57ee3ef4b60502db49a
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-06-05 17:49:35 +0200

    Show table and product groups (#6)

--- WaiterRobot/Ui/Order/ProductSearch.swift
+++ WaiterRobot/Ui/Order/ProductSearch.swift
@@ -7,20 +7,91 @@ struct ProductSearch: View {
   @ObservedObject var vm: ObservableViewModel<OrderState, OrderEffect, OrderViewModel>
   
   @State private var search: String = ""
+  @State private var selectedTab: Int = 0
+  
+  private let layout = [
+    GridItem(.adaptive(minimum: 110))
+  ]
   
   var body: some View {
     NavigationView {
-      List {
-        ForEach(vm.state.products, id: \.id) { product in
-          ProductListItem(product: product) {
-            vm.actual.addItem(product: product, amount: 1)
-            dismiss()
+      if(vm.state.productGroups.isEmpty) {
+        Text(S.productSearch.noProductFound())
+          .multilineTextAlignment(.center)
+          .frame(maxWidth: .infinity)
+          .padding()
+      } else {
+        VStack {
+          
+          ProducSearchTabBarHeader(currentTab: $selectedTab, tabBarOptions: getGroupNames(vm.state.productGroups))
+          
+          TabView(selection: $selectedTab) {
+            ScrollView {
+              LazyVGrid(columns: layout) {
+                ForEach(vm.state.productGroups, id: \.group.id) { groupWithProducts in
+                  if(!groupWithProducts.products.isEmpty) {
+                    Section {
+                      ForEach(groupWithProducts.products, id: \.id) { product in
+                        ProductListItem(product: product) {
+                          vm.actual.addItem(product: product, amount: 1)
+                          dismiss()
+                        }
+                        .foregroundColor(Color("textColor"))
+                        .padding(10)
+                      }
+                    } header: {
+                      HStack {
+                        Color(UIColor.lightGray).frame(height: 1)
+                        Text(groupWithProducts.group.name)
+                        Color(UIColor.lightGray).frame(height: 1)
+                      }
+                    }
+                  }
+                }
+                Spacer()
+              }
+              .padding()
+            }
+            .tag(0)
+            
+            ForEach(Array(vm.state.productGroups.enumerated()), id: \.element.group.id) { index, groupWithProducts in
+              ScrollView {
+                LazyVGrid(columns: layout, spacing: 0) {
+                  ForEach(groupWithProducts.products, id: \.id) { product in
+                    ProductListItem(product: product) {
+                      vm.actual.addItem(product: product, amount: 1)
+                      dismiss()
+                    }
+                    .foregroundColor(Color("textColor"))
+                    .padding(10)
+                  }
+                  Spacer()
+                }
+                .padding()
+              }
+              .tag(index + 1)
+            }
           }
-          .foregroundColor(Color("textColor"))
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always))
+        .onChange(of: search, perform: vm.actual.filterProducts)
+        .toolbar {
+          ToolbarItem(placement: .navigationBarLeading) {
+            Button(S.dialog.cancel()) {
+              dismiss()
+            }
+          }
         }
       }
-      .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always))
-      .onChange(of: search, perform: vm.actual.filterProducts)
     }
+  }
+  
+  private func getGroupNames(_ productGroups: Array<ProductGroupWithProducts>) -> Array<String> {
+    var groupNames = productGroups.map { groupWithProducts in
+      groupWithProducts.group.name
+    }
+    groupNames.insert(S.productSearch.allGroups(), at: 0)
+    return groupNames
   }
 }
