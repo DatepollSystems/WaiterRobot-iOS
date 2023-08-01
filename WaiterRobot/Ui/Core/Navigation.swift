b2508109cc202380ec7dd1c2cbdc9f5151921bc2
Alexander Kauer <contact@kaulex.dev>
2023-08-01 13:45:45 +0200

    fixed formatting

--- WaiterRobot/Ui/Core/Navigation.swift
+++ WaiterRobot/Ui/Core/Navigation.swift
@@ -39,12 +39,17 @@ extension View {
     }
 
     @MainActor
-    func handleSideEffects<S, E, VM, OVM>(of vm: OVM, _ navigator: UIPilot<Screen>, handler: ((E) -> Bool)? = nil) -> some View where S: ViewModelState, E: ViewModelEffect, VM: AbstractViewModel<S, E>, OVM: ObservableViewModel<S, E, VM> {
+    func handleSideEffects<S, E, VM, OVM>(
+        of vm: OVM, _ navigator: UIPilot<Screen>,
+        handler: ((E) -> Bool)? = nil
+    ) -> some View where S: ViewModelState, E: ViewModelEffect, VM: AbstractViewModel<S, E>, OVM: ObservableViewModel<S, E, VM> {
         onReceive(vm.sideEffect) { effect in
             debugPrint("Got Sideeffect \(effect)")
+
             switch effect {
             case let navEffect as NavOrViewModelEffectNavEffect<E>:
                 navigator.navigate(navEffect.action)
+
             case let sideEffect as NavOrViewModelEffectVMEffect<E>:
                 if handler?(sideEffect.effect) != true {
                     koin.logger(tag: "handleSideEffects").w { "Side effect \(sideEffect.effect) was not handled." }
