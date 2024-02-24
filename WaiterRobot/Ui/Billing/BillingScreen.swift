f065ad8b95d4b529c24f93e1d5dc56445cd399d7
Alexander Kauer <contact@kaulex.dev>
2024-02-24 21:35:56 +0100

    Got it running

--- WaiterRobot/Ui/Billing/BillingScreen.swift
+++ WaiterRobot/Ui/Billing/BillingScreen.swift
@@ -16,96 +16,102 @@ struct BillingScreen: View {
     }
 
     var body: some View {
-        ScreenContainer(viewModel.state) {
-            VStack {
-                List {
-                    if viewModel.state.billItems.isEmpty {
-                        Text(localize.billing.noOpenBill(value0: table.number.description, value1: table.groupName))
-                            .multilineTextAlignment(.center)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                    } else {
-                        Section {
-                            ForEach(viewModel.state.billItems, id: \.self) { item in
-                                BillListItem(
-                                    item: item,
-                                    addOne: {
-                                        viewModel.actual.addItem(id: item.productId, amount: 1)
-                                    },
-                                    addAll: {
-                                        viewModel.actual.addItem(id: item.productId, amount: item.ordered - item.selectedForBill)
-                                    },
-                                    removeOne: {
-                                        viewModel.actual.addItem(id: item.productId, amount: -1)
-                                    },
-                                    removeAll: {
-                                        viewModel.actual.addItem(id: item.productId, amount: -item.selectedForBill)
-                                    }
-                                )
-                            }
-                        } header: {
-                            HStack {
-                                Text("Ordered")
-                                Spacer()
-                                Text("Selected")
-                            }
+        content()
+            .navigationTitle(localize.billing.title(value0: table.number.description, value1: table.groupName))
+            .navigationBarTitleDisplayMode(.inline)
+//            .customBackNavigation(title: localize.dialog.cancel(), icon: nil, action: { viewModel.actual.goBack() }) // TODO: whatever?
+//            .confirmationDialog(localize.billing.notSent.title(), isPresented: Binding.constant(viewModel.state.showConfirmationDialog), titleVisibility: .visible) {
+//                Button(localize.dialog.closeAnyway(), role: .destructive, action: { viewModel.actual.abortBill() })
+//                Button(localize.dialog.cancel(), role: .cancel, action: { viewModel.actual.keepBill() })
+//            } message: {
+//                Text(localize.billing.notSent.desc())
+//            }
+//            TODO: Check if this was removed
+//            .refreshable {
+//                viewModel.actual.loadBill()
+//            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        viewModel.actual.selectAll()
+                    } label: {
+                        Image(systemName: "checkmark")
+                    }
+
+                    Button {
+                        viewModel.actual.unselectAll()
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                }
+            }
+            .sheet(isPresented: $showPayDialog) {
+                PayDialog(viewModel: viewModel)
+            }
+            .handleSideEffects(of: viewModel, navigator)
+    }
+
+    @ViewBuilder
+    func content() -> some View {
+        let billItems = Array(viewModel.state.billItemsArray)
+
+        VStack {
+            List {
+                if billItems.isEmpty {
+                    Text(localize.billing.noOpenBill(value0: table.number.description, value1: table.groupName))
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                } else {
+                    Section {
+                        ForEach(billItems, id: \.self) { item in
+                            BillListItem(
+                                item: item,
+                                addOne: {
+                                    viewModel.actual.addItem(id: item.productId, amount: 1)
+                                },
+                                addAll: {
+                                    viewModel.actual.addItem(id: item.productId, amount: item.ordered - item.selectedForBill)
+                                },
+                                removeOne: {
+                                    viewModel.actual.addItem(id: item.productId, amount: -1)
+                                },
+                                removeAll: {
+                                    viewModel.actual.addItem(id: item.productId, amount: -item.selectedForBill)
+                                }
+                            )
+                        }
+                    } header: {
+                        HStack {
+                            Text("Ordered")
+                            Spacer()
+                            Text("Selected")
                         }
                     }
                 }
-                .refreshable {
-                    viewModel.actual.loadBill()
-                }
+            }
 
-                HStack {
-                    Text("\(localize.billing.total()):")
-                    Spacer()
-                    Text("\(viewModel.state.priceSum)")
+            HStack {
+                Text("\(localize.billing.total()):")
+                Spacer()
+                Text("\(viewModel.state.priceSum)")
+            }
+            .font(.title2)
+            .padding()
+            .overlay(alignment: .bottom) {
+                Button {
+                    showPayDialog = true
+                } label: {
+                    Image(systemName: "dollarsign")
+                        .font(.system(.title))
+                        .padding()
+                        .tint(.white)
                 }
-                .font(.title2)
-                .padding()
-                .overlay(alignment: .bottom) {
-                    Button {
-                        showPayDialog = true
-                    } label: {
-                        Image(systemName: "dollarsign")
-                            .font(.system(.title))
-                            .padding()
-                            .tint(.white)
-                    }
-                    .background(.blue)
-                    .mask(Circle())
-                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
-                    .disabled(viewModel.state.viewState != ViewState.Idle.shared || !viewModel.state.hasSelectedItems)
-                }
+                .background(.blue)
+                .mask(Circle())
+                .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
+                .disabled(viewModel.state.viewState != ViewState.Idle.shared || !viewModel.state.hasSelectedItems)
             }
         }
-        .navigationTitle(localize.billing.title(value0: table.number.description, value1: table.groupName))
-        .navigationBarTitleDisplayMode(.inline)
-        .customBackNavigation(title: localize.dialog.cancel(), icon: nil, action: { viewModel.actual.goBack() }) // TODO:
-        .confirmationDialog(localize.billing.notSent.title(), isPresented: Binding.constant(viewModel.state.showConfirmationDialog), titleVisibility: .visible) {
-            Button(localize.dialog.closeAnyway(), role: .destructive, action: { viewModel.actual.abortBill() })
-            Button(localize.dialog.cancel(), role: .cancel, action: { viewModel.actual.keepBill() })
-        } message: {
-            Text(localize.billing.notSent.desc())
-        }
-        .toolbar {
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                Button {
-                    viewModel.actual.selectAll()
-                } label: {
-                    Image(systemName: "checkmark")
-                }
-
-                Button {
-                    viewModel.actual.unselectAll()
-                } label: {
-                    Image(systemName: "xmark")
-                }
-            }
-        }
-        .sheet(isPresented: $showPayDialog) {
-            PayDialog(viewModel: viewModel)
-        }
-        .handleSideEffects(of: viewModel, navigator)
     }
 }
--- WaiterRobot/Ui/Order/OrderScreen.swift
+++ WaiterRobot/Ui/Order/OrderScreen.swift
@@ -20,59 +20,59 @@ struct OrderScreen: View {
     var body: some View {
         ScreenContainer(viewModel.state) {
             ZStack {
-                VStack {
-                    if viewModel.state.currentOrder.isEmpty {
-                        Text(localize.order.addProduct())
-                            .multilineTextAlignment(.center)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                    } else {
-                        List {
-                            ForEach(viewModel.state.currentOrder, id: \.product.id) { orderItem in
-                                OrderListItem(
-                                    name: orderItem.product.name,
-                                    amount: orderItem.amount,
-                                    note: orderItem.note,
-                                    addOne: { viewModel.actual.addItem(product: orderItem.product, amount: 1) },
-                                    removeOne: { viewModel.actual.addItem(product: orderItem.product, amount: -1) },
-                                    removeAll: { viewModel.actual.removeAllOfProduct(productId: orderItem.product.id) },
-                                    onSaveNote: { note in
-                                        viewModel.actual.addItemNote(item: orderItem, note: note)
-                                    }
-                                )
-                            }
-                        }
-                    }
-                }
-
-                EmbeddedFloatingActionButton(icon: "plus") {
-                    showProductSearch = true
-                }
+//                VStack {
+//                    if viewModel.state.currentOrder.isEmpty {
+//                        Text(localize.order.addProduct())
+//                            .multilineTextAlignment(.center)
+//                            .frame(maxWidth: .infinity)
+//                            .padding()
+//                    } else {
+//                        List {
+//                            ForEach(viewModel.state.currentOrder, id: \.product.id) { orderItem in
+//                                OrderListItem(
+//                                    name: orderItem.product.name,
+//                                    amount: orderItem.amount,
+//                                    note: orderItem.note,
+//                                    addOne: { viewModel.actual.addItem(product: orderItem.product, amount: 1) },
+//                                    removeOne: { viewModel.actual.addItem(product: orderItem.product, amount: -1) },
+//                                    removeAll: { viewModel.actual.removeAllOfProduct(productId: orderItem.product.id) },
+//                                    onSaveNote: { note in
+//                                        viewModel.actual.addItemNote(item: orderItem, note: note)
+//                                    }
+//                                )
+//                            }
+//                        }
+//                    }
+//                }
+//
+//                EmbeddedFloatingActionButton(icon: "plus") {
+//                    showProductSearch = true
+//                }
             }
         }
-        .navigationTitle(localize.order.title(value0: table.number.description, value1: table.groupName))
-        .navigationBarTitleDisplayMode(.large)
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    viewModel.actual.sendOrder()
-                } label: {
-                    Image(systemName: "paperplane.fill")
-                }
-                .disabled(viewModel.state.currentOrder.isEmpty || viewModel.state.viewState != ViewState.Idle.shared)
-            }
-        }
-        .customBackNavigation(title: localize.dialog.cancel(), icon: "chevron.backward", action: { viewModel.actual.goBack() })
-        .confirmationDialog(localize.order.notSent.title(), isPresented: Binding.constant(viewModel.state.showConfirmationDialog), titleVisibility: .visible) {
-            Button(localize.dialog.closeAnyway(), role: .destructive, action: { viewModel.actual.abortOrder() })
-            Button(localize.order.keepOrder(), role: .cancel, action: { viewModel.actual.keepOrder() })
-        } message: {
-            Text(localize.order.notSent.desc())
-        }
-        .sheet(isPresented: $showProductSearch) {
-            ProductSearch(viewModel: viewModel)
-        }
-        .handleSideEffects(of: viewModel, navigator)
+//        .navigationTitle(localize.order.title(value0: table.number.description, value1: table.groupName))
+//        .navigationBarTitleDisplayMode(.large)
+//        .navigationBarBackButtonHidden()
+//        .toolbar {
+//            ToolbarItem(placement: .navigationBarTrailing) {
+//                Button {
+//                    viewModel.actual.sendOrder()
+//                } label: {
+//                    Image(systemName: "paperplane.fill")
+//                }
+//                .disabled(viewModel.state.currentOrder.isEmpty || viewModel.state.viewState != ViewState.Idle.shared)
+//            }
+//        }
+//        .customBackNavigation(title: localize.dialog.cancel(), icon: "chevron.backward", action: { viewModel.actual.goBack() })
+//        .confirmationDialog(localize.order.notSent.title(), isPresented: Binding.constant(viewModel.state.showConfirmationDialog), titleVisibility: .visible) {
+//            Button(localize.dialog.closeAnyway(), role: .destructive, action: { viewModel.actual.abortOrder() })
+//            Button(localize.order.keepOrder(), role: .cancel, action: { viewModel.actual.keepOrder() })
+//        } message: {
+//            Text(localize.order.notSent.desc())
+//        }
+//        .sheet(isPresented: $showProductSearch) {
+//            ProductSearch(viewModel: viewModel)
+//        }
+//        .handleSideEffects(of: viewModel, navigator)
     }
 }
