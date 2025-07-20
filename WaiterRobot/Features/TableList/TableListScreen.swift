93e73abea77f01a9bf92736c91a6ac0b6c76ed35
Fabian Schedler <fabian.schedler@studo.com>
2025-07-20 12:17:34 +0200

    Fix toolbar background

--- WaiterRobot/Features/TableList/TableListScreen.swift
+++ WaiterRobot/Features/TableList/TableListScreen.swift
@@ -31,7 +31,6 @@ struct TableListScreen: View {
                             }
                         }
                     }
-                    .toolbarBackground(.hidden, for: .navigationBar)
             } else {
                 content()
                     .toolbar {
